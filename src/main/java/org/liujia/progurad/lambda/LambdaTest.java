0095e4a65275ab7b3e8be3be8dbf5ccb887367c8
Jia Liu <liujiawinds@gmail.com>
2018-07-31 11:02:01 +0800

    first commit

---
+++ src/main/java/org/liujia/progurad/lambda/LambdaTest.java
@@ -0,0 +1,41 @@
+package org.liujia.progurad.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by liujia on 2018/7/31.
+ */
+public class LambdaTest {
+
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("123","fasdf", "1dfsfa", "gf");
+
+        System.out.println("test method reference");
+        list.forEach(A::print);
+
+        System.out.println("test map function");
+        List<String> collect = list.stream().map(v -> v + "12").collect(Collectors.toList());
+        for (String s : collect) {
+            System.out.println(s);
+        }
+
+        System.out.println("test interface implement");
+        B b = () -> A.print("123");
+        b.print();
+    }
+
+
+}
+
+class A {
+    static void print(String str) {
+        System.out.println(str);
+    }
+}
+
+interface B {
+    void print();
+}
